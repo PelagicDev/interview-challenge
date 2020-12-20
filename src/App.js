060d3cdbae92838d65858d9c1875799e9cbe7353
Jordan Godwin <pelagic.dev@gmail.com>
2020-12-20 13:07:56 -0500

    Call API & output data;
    Making use of useEffect & useState, we simply call our API, store the data in our movies in local state and then map over them to output them on the screen.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from 'react'
-import { Container } from '@chakra-ui/react'
+import React, { useState, useEffect } from 'react'
+import { Box, Container } from '@chakra-ui/react'
 
 import * as routes from './lib/routes'
 
 function App() {
-  return <Container maxW="6xl" padding={2}></Container>
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    fetch(routes.MOVIES)
+      .then((res) => res.json())
+      .then((result) => {
+        setMovies(result)
+      })
+  }, [])
+
+  return (
+    <Container maxW="6xl" padding={2} centerContent>
+      <Box>
+        {movies.map((movie) => (
+          <div>{movie.title}</div>
+        ))}
+      </Box>
+    </Container>
+  )
 }
 
 export default App
