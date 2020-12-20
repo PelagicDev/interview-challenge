45c5e2a7db9c579ae0d4fe9631a246d8efe48339
Jordan Godwin <pelagic.dev@gmail.com>
2020-12-20 13:26:59 -0500

    Add styling and beging component organization;

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { Box, Container } from '@chakra-ui/react'
+import { Box, Container, Heading, SimpleGrid, Text } from '@chakra-ui/react'
 
 import * as routes from './lib/routes'
 
@@ -15,14 +15,36 @@ function App() {
   }, [])
 
   return (
-    <Container maxW="6xl" padding={2} centerContent>
-      <Box>
-        {movies.map((movie) => (
-          <div>{movie.title}</div>
-        ))}
-      </Box>
-    </Container>
+    <Box bg="gray.300">
+      <Container maxW="6xl" centerContent>
+        <PageHeader />
+        <SimpleGrid columns={3} spacing={5} padding={5}>
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </SimpleGrid>
+      </Container>
+    </Box>
   )
 }
+
+const PageHeader = () => (
+  <Box bg="gray.600" w="100%" p={4} color="white" textAlign="center">
+    <Heading>Movies Database</Heading>
+  </Box>
+)
+
+const MovieCard = ({ movie }) => (
+  <Box
+    maxW="sm"
+    borderWidth="1px"
+    borderRadius="lg"
+    overflow="hidden"
+    padding={3}
+    borderColor="gray.600"
+  >
+    <Text>{movie.title}</Text>
+  </Box>
+)
 
 export default App
