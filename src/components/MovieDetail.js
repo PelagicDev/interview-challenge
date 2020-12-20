13c46aed268932abe41a786c5eaffafeb89be902
Jordan Godwin <pelagic.dev@gmail.com>
2020-12-20 14:41:54 -0500

    Add styling to the MovieDetail component

--- src/components/MovieDetail.js
+++ src/components/MovieDetail.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import {
+  Badge,
   Box,
   Button,
+  Divider,
+  Flex,
+  ListItem,
   Modal,
   ModalOverlay,
   ModalContent,
@@ -9,6 +13,8 @@ import {
   ModalCloseButton,
   ModalFooter,
   ModalBody,
+  Text,
+  UnorderedList,
   useDisclosure,
 } from '@chakra-ui/react'
 
@@ -24,12 +30,23 @@ const MovieDetail = ({ movie }) => {
       <Modal size="xl" isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>{movie.title}</ModalHeader>
+          <ModalHeader>
+            <Flex w="90%">
+              <Text fontSize="xl">{movie.title}</Text>
+              <Box marginLeft={4}>
+                <Badge
+                  ml="1"
+                  fontSize="0.8em"
+                  variant="outline"
+                  colorScheme="green"
+                >
+                  {movie.year}
+                </Badge>
+              </Box>
+            </Flex>
+          </ModalHeader>
           <ModalCloseButton />
-          <ModalBody>
-            <p>{movie.description}</p>
-            <p>{movie.year}</p>
-          </ModalBody>
+          <MovieContent movie={movie} />
 
           <ModalFooter>
             <Button colorScheme="blue" mr={3} onClick={onClose}>
@@ -39,6 +56,46 @@ const MovieDetail = ({ movie }) => {
         </ModalContent>
       </Modal>
     </Box>
+  )
+}
+
+const MovieContent = ({ movie }) => {
+  const { description, stars, writers, directors } = movie
+
+  return (
+    <ModalBody fontSize="lg">
+      <p>{description}</p>
+      <Divider margin={3} />
+
+      <Box>
+        <Text fontSize="lg" as="u">
+          Starring:
+        </Text>
+        {stars.map((star) => (
+          <UnorderedList spacing={3}>
+            <ListItem>
+              <Text>
+                {star.actor} as <Text as="i">{star.character}</Text>
+              </Text>
+            </ListItem>
+          </UnorderedList>
+        ))}
+      </Box>
+      <Divider margin={3} />
+
+      <Box>
+        <Text as="u" marginRight={2}>
+          Directors:
+        </Text>
+        <Text as="i">{directors.join(', ')}</Text>
+      </Box>
+      <Box>
+        <Text as="u" marginRight={2}>
+          Writers:
+        </Text>
+        <Text as="i">{writers.join(', ')}</Text>
+      </Box>
+    </ModalBody>
   )
 }
 
